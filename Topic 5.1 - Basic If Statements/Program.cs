dadd3a16b8a2df86f63fad5ab5135ee355f00a28
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-20 09:22:51 -0400

    Finished

--- Topic 5.1 - Basic If Statements/Program.cs
+++ Topic 5.1 - Basic If Statements/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 int people = 30;
 int cats = 30;
 int dogs = 15;
@@ -35,4 +37,58 @@ if (people <= dogs)
 if (people == dogs)
 {
     Console.WriteLine("People are dogs.");
+}
+Console.WriteLine();
+string dinosaur;
+Console.WriteLine("What famous dinosaur has three large horns?");
+dinosaur = Console.ReadLine();
+if (dinosaur.ToLower() == "triceratops")
+    Console.WriteLine("You are correct.");
+
+string magicWord;
+Console.Write("Please say the magic word: ");
+magicWord = Console.ReadLine();
+if (magicWord.ToLower() == "please")
+    Console.WriteLine("You're welcome.");
+
+string name;
+int age;
+Console.Write("Hey, what's your name? ");
+name = Console.ReadLine();
+Console.WriteLine();
+Console.Write("Ok, " + name + ", how old are you?  ");
+int.TryParse(Console.ReadLine(), out age);
+Console.WriteLine();
+
+if (age < 16)
+{
+    Console.WriteLine("You can't drive.");
+}
+if (age < 18)
+{
+    Console.WriteLine("You can't vote.");
+}
+if (age < 25)
+{
+    Console.WriteLine("You can't rent a car.");
+}
+if (age >= 25)
+{
+    Console.WriteLine("You can do anything that is legal.");
+}
+
+int temperature;
+Console.WriteLine("Please enter the freezing temperature of water: ");
+int.TryParse(Console.ReadLine(), out temperature);
+if (temperature == 0)
+{
+    Console.WriteLine("Ahh yes, 0 degrees Celsius is correct.");
+}
+if (temperature == 32)
+{
+    Console.WriteLine("Ahh yes, 32 degrees Fahrenheit is correct.");
+}
+if (temperature == 273)
+{
+    Console.WriteLine("Ahh yes, 273.2 degrees Kelvin to be precise is correct.");
 }
